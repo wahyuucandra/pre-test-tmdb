2fc1198bb8103a43e18121405b2f03ea0b9b83ec
wahyuucandra <wahyuucandra@gmail.com>
2024-07-17 10:36:48 +0700

    fix: favorite and link detail movie

--- src/components/molecules/MovieCard.tsx
+++ src/components/molecules/MovieCard.tsx
@@ -1,42 +1,54 @@
 import { IMovie } from "@/interface/movie";
 import React from "react";
 import Image from "next/image";
-import moment from "moment"
+import moment from "moment";
 import { MdOutlineFavorite } from "react-icons/md";
 import Link from "next/link";
 
-const MovieCard = ({ 
-    movies, 
-    setFavoriteMovie
-} : { 
-    movies: IMovie[] | undefined, 
-    setFavoriteMovie: (id: number) => void
+const MovieCard = ({
+  movies,
+  setFavoriteMovie,
+}: {
+  movies: IMovie[] | undefined;
+  setFavoriteMovie: (id: number) => void;
 }) => {
-
   return (
-    <div className="grid md:grid-cols-4 lg:grid-cols-6 gap-3">
-        {
-            movies?.map((movie: IMovie) => (
-                <Link href={`/movie?movie_id=${movie.id}`} key={movie.id} className="relative bg-white rounded-lg border border-gray-400 shadow-md w-[220px]"> 
-                    <Image 
-                        src={`https://media.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`}
-                        width={220}
-                        height={330}
-                        alt=""
-                        className="rounded-t-lg"
-                    />
-                    <div className="content__card p-3 flex flex-col">
-                        <div className="font-semibold">{movie.title}</div>
-                        <div className="text-sm text-gray-500">{moment(movie?.release_date).format('MMM DD, yyyy')}</div>
-                    </div>
-                    <button className="absolute top-[10px] right-[10px]" onClick={() => setFavoriteMovie(movie.id)}>
-                        <MdOutlineFavorite color={movie.is_favourite ? "red" : "gray"} size={30} className="drop-shadow-sm"/>
-                    </button>
-                </Link>
-            ))
-        }
+    <div className="grid md:grid-cols-4 lg:grid-cols-5 gap-6">
+      {movies?.map((movie: IMovie) => (
+        <div
+          key={movie.id}
+          className="relative bg-white rounded-lg border border-gray-400 shadow-md w-[220px]"
+        >
+          <Link href={`/movie?movie_id=${movie.id}`}>
+            <Image
+              src={`https://media.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`}
+              width={220}
+              height={330}
+              alt=""
+              className="rounded-t-lg"
+            />
+            <div className="content__card p-3 flex flex-col">
+              <div className="font-semibold">{movie.title}</div>
+              <div className="text-sm text-gray-500">
+                {moment(movie?.release_date).format("MMM DD, yyyy")}
+              </div>
+            </div>
+          </Link>
+
+          <button
+            className="absolute top-[10px] right-[10px]"
+            onClick={() => setFavoriteMovie(movie.id)}
+          >
+            <MdOutlineFavorite
+              color={movie.is_favourite ? "red" : "gray"}
+              size={30}
+              className="drop-shadow-sm"
+            />
+          </button>
+        </div>
+      ))}
     </div>
-  )
+  );
 };
 
 export default MovieCard;
